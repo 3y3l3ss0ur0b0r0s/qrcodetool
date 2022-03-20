3a0dfc4fcd3fa6e347e14a103494f57620422a64
3y3l3ss0ur0b0r0s <danielle.bertulfo@gmail.com>
2022-03-19 22:40:44 -0400

    Add files via upload

---
+++ qrcodetool.py
@@ -0,0 +1,132 @@
+# QR Code Tool is a beginner practice project that will help develop knowledge of:
+    # Installing and using multiple modules
+    # https://www.youtube.com/watch?v=SqvVm3QiQVk&t=3192s
+
+    # img=qrcode.make(data)
+
+import os
+import qrcode
+from os.path import exists
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+# Encoding a QR code
+#data='You\'ve found a hidden Sorb...' # The data to encode into QR code
+
+#qr=qrcode.QRCode(version=1, box_size=6, border=2)
+
+#qr.add_data(data)
+
+#qr.make(fit=True)
+#img=qr.make_image(fill_color='#170813',back_color='#9c5954')
+
+#img.save('C:/Users/danie/Documents/coding/Python/beginner projects/qrcodetool_outputs/generated_qrcode_02.png')
+
+# Decoding a QR Code
+#img=Image.open('C:/Users/danie/Documents/coding/Python/beginner projects/qrcodetool_outputs/generated_qrcode.png')
+
+#result=decode(img)
+
+#print(result)
+
+def use_tool():
+    dir=os.path.dirname(__file__)
+    path=r'{}/QR Code Tool Outputs'.format(dir)
+
+    try:
+        os.makedirs(path)
+    except OSError:
+        pass
+
+    stop_program=False
+    valid_response=False
+    response_confirmed=False
+
+    while stop_program==False:
+        print("\n\t============================\n\t\tQR Code Tool\n\t============================")
+        user_input=input("\n\tEnter 1 to encode a QR code.\n\tEnter 2 to decode a QR code.\n\tEnter 3 to exit.\n\t")
+        if user_input=='1':
+            qr=qrcode.QRCode(version=1,box_size=10,border=2)
+
+            while not response_confirmed:
+                data=input("\n\tWhat do you want to encode?\n\t")
+                answer=input("\n\tAre you sure? Enter 1 for yes, enter 2 for no.\n\t")
+                if answer=='1':
+                    response_confirmed=True
+                elif answer=='2':
+                    continue
+                else:
+                    continue
+                qr.add_data(data)
+
+            response_confirmed=False
+            while not response_confirmed:
+                fill=input("\n\t6-digit hex code for fill color? Don't include the # symbol.\n\t")
+                answer=input("\n\tAre you sure? Enter 1 for yes, enter 2 for no.\n\t")
+                if answer=='1':
+                    while not valid_response:
+                        if len(fill)!=6:
+                            print("\n\tThe hex code must be 6-digits.")
+                            fill=input("\n\t6-digit hex code for fill color? Don't include the # symbol.\n\t")
+                        else:
+                            valid_response=True
+                        response_confirmed=True
+                elif answer=='2':
+                    continue
+                else:
+                    continue
+
+                valid_response=False
+                response_confirmed=False
+                back=input("\n\t6-digit hex code for background color? Don't include the # symbol.\n\t")
+                answer=input("\n\tAre you sure? Enter 1 for yes, enter 2 for no.\n\t")
+                if answer=='1':
+                    while not valid_response:
+                        if len(back)!=6:
+                            print("\n\tThe hex code must be 6-digits.")
+                            fill=input("\n\t6-digit hex code for back color? Don't include the # symbol.\n\t")
+                        else:
+                            valid_response=True
+                        response_confirmed=True
+                elif answer=='2':
+                    continue
+                else:
+                    continue
+
+                img=qr.make_image(fill_color='#'+fill,back_color='#'+back)
+
+                image_name=input("\n\tWhat do you want to call it?\n\t")
+                location=path+'/'+image_name+'.png'
+
+                img.save(location)
+
+        elif user_input=='2':
+            response_confirmed=False
+            valid_response=False
+            while not response_confirmed:
+                file_path=input("\n\tWhat do you want to decode?\n\t")
+                answer=input("\n\tAre you sure? Enter 1 for yes, enter 2 for no.\n\t")
+                if answer=='1':
+                    while not valid_response:
+                        file_path=file_path.replace('\"','')
+                        if os.path.exists(file_path)==True:
+                            img=Image.open(file_path)
+                            result=decode(img)
+                            print("\n\tDecoded QR code:","\n\t",result)
+                            valid_response=True
+                        else:
+                            file_path=input("\n\tThat file doesn't exist...try a different one? Include the path, without quotation marks.\n\t")
+                            continue
+                        response_confirmed=True
+                elif answer=='2':
+                    continue
+                else:
+                    continue
+
+        elif user_input=='3':
+            stop_program=True
+        else:
+            print("\nThat was not valid input. Try again, please!")
+
+if __name__=='__main__':
+    use_tool()
